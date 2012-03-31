268cce8d0f48e367113f47b1a335408e204fd602
jgaracci <jgaracci@yahoo.com>
2012-03-31 10:15:37 -0500

    added "prizes list" to see all prizes

--- mclogcmdexec/src/org/titanomachia/mclogcmdexec/command/Prizes.java
+++ mclogcmdexec/src/org/titanomachia/mclogcmdexec/command/Prizes.java
@@ -16,40 +16,20 @@ public class Prizes extends Command {
 	@Override
 	public void execute() {
 		Integer points = ApplicationContext.getValue("quiz.points." + getUser());
-		
-		if (null == points || 0 == points) {
-			CommandUtils.writeToConsole("You have no points", getUser());
-			return;
-		}
-		
-		Map<String, Prize> prizes = new HashMap<String, Prize>();
-		Properties prizeProperties = new Properties();
-		try {
-			prizeProperties.load( new FileReader("prizes.properties") );
-			for (Object key : prizeProperties.keySet()) {
-				String id = String.valueOf(key).toUpperCase();
-				
-				String value = (String)prizeProperties.get(key);
-				String name = value.substring(0, value.indexOf("("));
-				Integer count = Integer.valueOf(value.substring(value.indexOf("(") + 1, value.indexOf(")")));
-				Integer typeId = Integer.valueOf(value.substring(value.indexOf("[") + 1, value.indexOf("]")));
-				Integer cost = Integer.valueOf(value.substring(value.indexOf("=") + 1));
-				if (points >= cost) {
-					prizes.put(id, new Prize(count, name, typeId, cost));
-				}
-			}
-		} catch (FileNotFoundException e) {
-			System.err.println("Failed to load prizes " + e);
-			CommandUtils.writeToConsole("Sorry, prizes not available", getUser());
-			return;
-		} catch (IOException e) {
-			System.err.println("Failed to load prizes " + e);
-			CommandUtils.writeToConsole("Sorry, prizes not available", getUser());
-			return;
-		}
+
+		Map<String, Prize> prizes = getPrizes(points, "list".equals(getArgs().trim()));
 		
 		if (getArgs().trim().length() == 0) {
 			showAvailablePrizes(points, prizes);
+			return;
+		}
+		else if ("list".equals(getArgs().trim())) {
+			showAllPrizes(prizes);
+			return;
+		}
+		
+		if (null == points || 0 == points) {
+			CommandUtils.writeToConsole("You have no points", getUser());
 			return;
 		}
 		
@@ -108,6 +88,35 @@ public class Prizes extends Command {
     	
     	CommandUtils.writeToConsole("You now have " + points + " point" + (points == 1 ? "" : "s"), getUser());
 	}
+
+	private Map<String, Prize> getPrizes(Integer points, boolean all) {
+		Map<String, Prize> prizes = new HashMap<String, Prize>();
+		try {
+			Properties prizeProperties = new Properties();
+			prizeProperties.load( new FileReader("prizes.properties") );
+			for (Object key : prizeProperties.keySet()) {
+				String id = String.valueOf(key).toUpperCase();
+				
+				String value = (String)prizeProperties.get(key);
+				String name = value.substring(0, value.indexOf("("));
+				Integer count = Integer.valueOf(value.substring(value.indexOf("(") + 1, value.indexOf(")")));
+				Integer typeId = Integer.valueOf(value.substring(value.indexOf("[") + 1, value.indexOf("]")));
+				Integer cost = Integer.valueOf(value.substring(value.indexOf("=") + 1));
+				if (all || points >= cost) {
+					prizes.put(id, new Prize(count, name, typeId, cost));
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("Failed to load prizes " + e);
+			CommandUtils.writeToConsole("Sorry, prizes not available", getUser());
+			return prizes;
+		} catch (IOException e) {
+			System.err.println("Failed to load prizes " + e);
+			CommandUtils.writeToConsole("Sorry, prizes not available", getUser());
+			return prizes;
+		}
+		return prizes;
+	}
     
     private void showAvailablePrizes(Integer points, Map<String, Prize> prizes) {
     	CommandUtils.writeToConsole("You have " + points + " point" + (points == 1 ? "" : "s") + ", prizes available:", getUser());
@@ -125,6 +134,22 @@ public class Prizes extends Command {
     	}
     	else {
     		CommandUtils.writeToConsole("Sorry, you need more points", getUser());
+    	}
+	}
+    
+    private void showAllPrizes(Map<String, Prize> prizes) {
+    	CommandUtils.writeToConsole("Prize list:", getUser());
+    	
+    	if (prizes.keySet().size() > 0) {
+	    	List<String> ids = new ArrayList<String>(prizes.keySet());
+	    	Collections.sort(ids);
+	    	
+	    	for (String id : ids) {
+	    		Prize prize = prizes.get(id);
+	    		CommandUtils.writeToConsole(id + " -> " + prize.count + " " + prize.name + " for " + prize.cost, getUser());
+	    	}
+	    	
+	    	CommandUtils.writeToConsole("Remember, purchasing ends your streak", getUser());
     	}
 	}
 
